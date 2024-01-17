fada753517d29f5fe746f2c6a637dd4f5eca28c3
Patrick Opie <opiepat@gmail.com>
2024-01-17 14:30:56 -0800

    Sync still needs to be fixed

--- packages/app/test.js
+++ packages/app/test.js
@@ -1,14 +1,16 @@
-import { fileURLToPath } from "node:url";
-import path from "node:path";
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
 
 const modulePath = fileURLToPath(import.meta.url);
 const cwd = path.dirname(modulePath);
 
-await import(path.join(cwd, 'test-sync.js'));
-console.log('✅ Tested Sync');
-await import(path.join(cwd,'test-run.js'));
-console.log('✅ Tested Run');
-await import(path.join(cwd,'test-dev.js'));
+(async () => {
+  await import(path.join(cwd, 'test-sync.js'));
+  console.log('✅ Tested Sync');
+  await import(path.join(cwd, 'test-run.js'));
+  console.log('✅ Tested Run');
+  await import(path.join(cwd, 'test-dev.js'));
 // await import('./test-dev-local.js');
-console.log('✅ Tested Dev');
-console.log('✅ Done');
+  console.log('✅ Tested Dev');
+  console.log('✅ Done');
+})();
