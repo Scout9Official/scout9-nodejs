e9170a9a53f03c24e5586091232c692aae1f46aa
Patrick Opie <opiepat@gmail.com>
2024-01-17 14:32:40 -0800

    Upgrade library with Window OS path module fixes

--- packages/app/build/index.js
+++ packages/app/build/index.js
@@ -2,7 +2,7 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-var index = require('./index-2c959dff.js');
+var index = require('./index-0d747abf.js');
 require('fs');
 require('events');
 require('path');
